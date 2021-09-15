f9f7aa285df83ad80b5045ac67ac8cdc7c43c1ef
Mikita Stankiewicz <itsmikita@gmail.com>
2021-09-15 04:52:13 +0300

    Fixes

--- public/assets/scripts/site.js
+++ public/assets/scripts/site.js
@@ -3,14 +3,16 @@
     fetch( "/api/media" )
       .then( response => response.json() )
       .then( response => {
-        let next = 0;
-        let interval = setInterval( () => {
-          if( next > response.length - 1 ) {
-            next = 0;
-          }
-          document.body.style.backgroundImage = "url('" + response[ next ].url + "')";
-          next++;
-        }, 87 );
+        if( "undefined" != typeof response[ 0 ] ) {
+          let next = 0;
+          let interval = setInterval( () => {
+            if( next > response.length - 1 ) {
+              next = 0;
+            }
+            document.body.style.backgroundImage = "url('" + response[ next ].url + "')";
+            next++;
+          }, 87 );
+        }
       } )
       .catch( error => {
         console.error( error );
